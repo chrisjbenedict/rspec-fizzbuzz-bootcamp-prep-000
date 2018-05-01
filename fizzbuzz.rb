2d427a0db385537baa30c23d6a3f3a89086559b2
Christopher Benedict <chrisjbenedict@gmail.com>
2018-05-01 21:42:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
-def fizzbuzz
+def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 == 0 && int % 5 == 0
-    "Fizzbuzz"
+    "FizzBuzz"
   else
     "nil"
+  end
 end
